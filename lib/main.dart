f7cbb0608ff2681cbe9939f0123d795b7728e6d3
k-a-v-g <kavyagoel09@gmail.com>
2022-01-15 01:02:09 +0530

    Weather App

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
+import 'dart:convert';
+
+void main() {
+  runApp(const MaterialApp());
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Future<Position> getPosition() async {
+    Position position = await Geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    return position;
+  }
+  String _locality = '';
+
+  Future<Placemark> getPlacemark(double latitude, double longitude) async {
+    List<Placemark> placemark = await placemarkFromCoordinates(latitude, longitude);
+    return placemark[0];
+  }
+  String _weather = '';
+
+  Future<String> getData(double latitude, double longitude) async {
+    String api = 'http://api.openweathermap.org/data/2.5/forecast';
+    String appId = '<aa67b32def20354807341ee27ff707e8>';
+
+    var url = Uri.parse('$api?lat=$latitude&lon=$longitude&APPID=$appId');
+
+    http.Response response = await http.get(url);
+
+    Map parsed = json.decode(response.body);
+
+    return parsed['list'][0]['weather'][0]['description'];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPosition().then((position) {
+      getPlacemark(position.latitude, position.longitude).then((data) {
+        getData(position.latitude, position.longitude).then((weather) {
+          setState(() {
+            _locality = data.locality!;
+            _weather = weather;
+          });
+        });
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Weather'),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey[700],
+        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            '$_locality',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          Text(
+            '$_weather',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ],
+      )
+    );
+  }
+}
+
